afa394eceb4bb2405465d0b197662711572bf8fd
ndmanh1000 <157332604+ndmanh1000@users.noreply.github.com>
2025-09-08 09:52:24 +0700

    cm

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -24,13 +24,13 @@ const Hero = () => {
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="#"
-                    className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
+                    className="rounded-md bg-[#053cc0] px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     🔥 Mua ngay
                   </Link>
                   <Link
                     href="#"
-                    className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
+                    className="inline-block rounded-md bg-[#ffc600] px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-[#ffc600]/80"
                   >
                     Đăng ký trải nghiệm
                   </Link>
